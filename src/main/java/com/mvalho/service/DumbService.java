cb410482e20e442b5f496492d8d63d52eebd09d9
Marcelo Carvalho <marceloca@scopus.com.br>
2016-09-01 17:09:29 -0300

    Added comment to DumbService

--- src/main/java/com/mvalho/service/DumbService.java
+++ src/main/java/com/mvalho/service/DumbService.java
@@ -1,5 +1,10 @@
 package com.mvalho.service;
-
+/**
+ * 
+ * We used it in the first post, we are not going to use it anymore.
+ * {@link http://www.mvalho.com/blog/2016/08/27/easy-your-life-with-spring-boot/}
+ *
+ */
 public interface DumbService {
 	public String doSomething(String string);
 }
